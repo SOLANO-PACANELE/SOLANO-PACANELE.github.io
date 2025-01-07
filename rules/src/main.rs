cde133c90adf5bcff93b13387ff40e8fb75336ae
Your Name <you@example.com>
2025-01-07 22:42:49 +0200

    msg

--- rules/src/main.rs
+++ rules/src/main.rs
@@ -1,4 +1,27 @@
-use rules::rule_set::random_prob_space;
+use rules::rule_set::RuleSet;
 fn main() {
-    println!("{:?}", random_prob_space(16));
+
+
+    for i in 5..=15 {
+        let i = i as f64 / 10.0;
+        
+        let p = RuleSet::random_rule_set(i).play_monte_carlo(100000);
+        println!("I(dorit) = {} , P(primit) = {}", i, p);
+
+    }
+    let r =  RuleSet::random_rule_set(0.96);
+    println!("{:?}",r);
+
+    
+    r.play_monte_carlo(10);
+    r.play_monte_carlo(100);
+    r.play_monte_carlo(1000);
+    r.play_monte_carlo(10000);
+    r.play_monte_carlo(100000);
+    r.play_monte_carlo(1000000);
+    r.play_monte_carlo(3000000);
+    r.play_monte_carlo(6000000);
+    r.play_monte_carlo(10000000);
+
+
 }
--- rules/src/rule_set.rs
+++ rules/src/rule_set.rs
@@ -1,12 +1,14 @@
 use std::collections::HashMap;
 
-use rand::Rng;
+use rand::{thread_rng, Rng};
 
 use crate::get_all_fruits;
 
-pub(crate) struct RuleSet {
-    prob: HashMap<&'static str, f64>,
-    reward: HashMap<(&'static str, u8), u16>,
+#[derive(Debug)]
+pub struct RuleSet {
+    prob: HashMap<&'static str, u16>,
+    rewards: HashMap<(&'static str, u8), u16>,
+    wheel_count: u8,
 }
 
 pub fn random_prob_space(len: usize) -> Vec<u16> {
@@ -43,13 +45,112 @@ pub fn random_prob_space(len: usize) -> Vec<u16> {
     some_prob
 }
 
+
+fn get_random_index_per_density(mut rand_val: u16, prob: & HashMap<&'static str, u16>) -> &'static str {
+    assert!(prob.len() > 2);
+    assert!(prob.len() < 200);
+
+    for i in prob.keys() {
+        if rand_val <= prob[i] {
+            return *i;
+        }
+        rand_val -= prob[i];
+    }
+    return prob.keys().next().unwrap();
+}
+/// get_prob(p, "banana", 1, 3) = prob that banana hits 1 time = p(banana, x, x) + p(x, banana, x) + p(x, x, banana), x != banana
+///     p(x) = 1 - prob[fruit]
+///     p("banana") = prob[fruit]
+///  p(banana, 1, 3) = C(3, 1) * p(banana)^1 * p(x)^2
+///  p(banana, 2, 3) = C(3, 2) * p(banana)^2 * p(x)^1
+///  p(banana, 3, 3) = C(3, 3) * p(banana)^3 * p(x)^0
+/// get_prob(p, "banana", 2, 3)
+/// get_prob(p, "banana", 3, 3)
+fn get_prob_for_index_and_density( prob: & HashMap<&str, u16>, fruit: &str, count: u8, total: u8) -> f64 {
+    assert!(count > 0);
+    assert!(count <= total);
+    assert!(total > 1);
+
+    let p_fruit = prob[fruit] as f64 / std::u16::MAX as f64;
+    let p_x = 1.0 - p_fruit;
+
+    let comb = combinari(total, count) as f64;
+
+    comb * p_fruit.powi(count as i32) * p_x.powi(total as i32 - count as i32)
+}
+
+
+fn factorial(n: u8) -> u64 {
+    let mut t = 1_u64;
+    for i in 1..=n {
+        t *= i as u64;
+    }
+    t
+}
+
+fn combinari(n: u8, k: u8) -> u64 {
+    factorial(n) / (factorial(k) * factorial(n-k))
+}
+
 impl RuleSet {
-    // fn random() -> Self {
-    //     let fruit = get_all_fruits();
+    pub fn play_monte_carlo(&self, count: u32) -> f64 {
+        let mut reward_total: f64 = 0.;
+        for _i in 0..count {
+            reward_total += self.play_random() as f64;
+        }
+        let avg_reward = reward_total / count as f64;
+        println!("avg_reward={avg_reward}  N={count}");
+        avg_reward
+    }
+    pub fn play_random(&self) -> u16 {
+        let r: [u16; 3] = thread_rng().gen();
+        self.play_random_from_seed(r)
+    }
+    pub fn play_random_from_seed(&self, random_seed: [u16;3]) -> u16 {
+        let mut result = vec![];
+        let mut fruit_hits = HashMap::new();
+        for i in 0..3 {
+            let x = get_random_index_per_density(random_seed[i], &self.prob);
+            result.push(x);
+            let old_val = fruit_hits.get(&x);
+            let new_val = if let Some(old_val) = old_val {old_val+ 1} else {1};
+            fruit_hits.insert(x, new_val);
+        }
 
+        // println!("hash = {fruit_hits:?}");
+        let mut reward: u32 = 0;
+        for (fruit, count) in fruit_hits.iter() {
+            let rrr = *self.rewards.get(&(*fruit, *count)).unwrap_or(&0);
+            reward += rrr as u32;
+        }
 
-        
+        reward.clamp(0, 55666) as u16
+    }
+    pub fn random_rule_set(desired_pay: f64) -> Self {
+        assert!(desired_pay >= 0.5);
+        assert!(desired_pay <= 2.0);
 
-    //     RuleSet { prob, reward }
-    // }
+        let wheel_count = 3;
+
+        let fruits = get_all_fruits();
+        let prob = random_prob_space(fruits.len());
+        let prob: HashMap<&str, u16> = HashMap::from_iter(fruits.iter().cloned().zip(prob.iter().cloned()));
+
+        let mut rewards = HashMap::new();
+        for fruit in fruits {
+            let p_fruit = prob[fruit] as f64 / std::u16::MAX as f64;
+            for score in 2..=wheel_count {
+                let prob = get_prob_for_index_and_density(&prob, fruit, score, wheel_count);
+                assert!(prob > 0.0);
+                assert!(prob < 1.0);
+                let max_reward =( 1.0 / prob * p_fruit / 2.0 ).clamp(0.0, 55666.0);
+                let max_reward_i = max_reward as u64;
+                let r_u16 = max_reward_i.clamp(0, 55666) as u16;
+                // println!("{fruit}x{score}   =>>>   reward_f: {max_reward}, reward_i: {max_reward_i}, reward_u16 = {r_u16}");
+                rewards.insert((fruit, score), r_u16 );
+            }
+        }
+
+        RuleSet { prob, rewards: rewards, wheel_count }
+    }
 }
