e27796570d5cf698a9c640eeb660a5b058dce7b4
johnny-smitherson <127537716+johnny-smitherson@users.noreply.github.com>
2025-01-12 18:53:17 +0200

    404

--- frontend/release.sh
+++ frontend/release.sh
@@ -7,3 +7,6 @@ dx build --release --platform web --package web_pacanele
 
 rm -rf ../.github_pages
 cp -a target/dx/web_pacanele/release/web/public/ ../.github_pages
+cd ../.github_pages
+echo '/* /index.html 200' > _redirects
+cp index.html 404.html
